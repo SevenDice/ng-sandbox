ebd03a311ad5988ee25b56ef1dc2b85e5402ac43
Kirill Gurin <benford.mark783@gmail.com>
2020-11-02 17:41:32 +0400

    feat: add Subject to 02-ng-app instead EventEmitter in shopping-list

--- 02-ng-app/src/app/shopping-list/shopping-list.component.ts
+++ 02-ng-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -8,14 +9,20 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+
+  private igChangeSub: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
+  }
+
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.igChangeSub = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
